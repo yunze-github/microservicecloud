15b58871a055b84a4eb4d481ff6ea78e9495a14f
yunze-github <yunze_work@163.com>
2019-12-05 21:45:36 +0800

    springcloud项目实例搭建 父工程microsevicecloud 通用API工程microservicecloud-api 服务提供微服务microservice-provider-dept  微服务消费microservice-consumer-dept eureka服务器微服务microservice-eureka

---
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/controller/DeptController_Consumer.java
@@ -0,0 +1,41 @@
+package com.atguigu.springcloud.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.atguigu.springcloud.entities.Dept;
+
+@RestController
+@RequestMapping("/customer/dept")
+public class DeptController_Consumer {
+
+	// 统一RestTemplate访问的url
+	private static final String REST_URL_PREFIX = "http://localhost:8001";
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public boolean add(Dept dept) {
+		return restTemplate.postForObject(REST_URL_PREFIX + "/dept/add", dept, Boolean.class);
+	}
+
+	@RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
+	public Dept get(@PathVariable Long id) {
+		return restTemplate.getForObject(REST_URL_PREFIX + "/dept/get/" + id, Dept.class);
+	}
+
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public List<Dept> list() {
+		return restTemplate.getForObject(REST_URL_PREFIX + "/dept/list", List.class);
+	}
+
+}

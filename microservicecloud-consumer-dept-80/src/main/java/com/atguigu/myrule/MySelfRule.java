5cce1a58fc50bee65370b1be4ba47ca88b21360b
yunze-github <yunze_work@163.com>
2019-12-06 21:18:52 +0800

    Ribbon自定义IRule轮询规则

---
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/myrule/MySelfRule.java
@@ -0,0 +1,17 @@
+package com.atguigu.myrule;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+
+@Configuration
+public class MySelfRule {
+	
+	@Bean
+	public IRule myRule() {
+		return new RandomRule();//Ribbon默认是轮训，我自定义为随机
+	}
+
+}

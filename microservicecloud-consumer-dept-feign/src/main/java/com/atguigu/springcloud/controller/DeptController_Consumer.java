6b191184279779415aa1054c94501e1c76b331a3
yunze-github <yunze_work@163.com>
2019-12-06 22:54:51 +0800

    使用Fegin负载均衡

---
+++ microservicecloud-consumer-dept-feign/src/main/java/com/atguigu/springcloud/controller/DeptController_Consumer.java
@@ -0,0 +1,39 @@
+package com.atguigu.springcloud.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.atguigu.springcloud.entities.Dept;
+import com.atguigu.springcloud.service.DeptClientService;
+
+@RestController
+@RequestMapping("/consumer/dept")
+public class DeptController_Consumer {
+
+	@Autowired
+	private DeptClientService service;
+	
+	@RequestMapping(value = "/get/{id}")
+	  public Dept get(@PathVariable("id") Long id)
+	  {
+	   return this.service.get(id);
+	  }
+	 
+	  @RequestMapping(value = "/list")
+	  public List<Dept> list()
+	  {
+	   return this.service.list();
+	  }
+	 
+	  @RequestMapping(value = "/add")
+	  public Object add(Dept dept)
+	  {
+	   return this.service.add(dept);
+	  }
+
+	
+}
